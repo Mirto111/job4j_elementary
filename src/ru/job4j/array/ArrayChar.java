c6560274e4a42201ef322cacbcdb0d7396dddbf8
Mirto111 <mitritdil@ya.ru>
2020-03-19 22:26:09 +0300

    6.4. Слово начинается с ...[#255735]  Убрал проверку входящих обьектов

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,10 +4,6 @@ public class ArrayChar {
 
   public static boolean startsWith(char[] word, char[] pref) {
     boolean result = true;
-    if (pref.length > word.length) {
-      return false;
-    }
-
     for (int i = 0; i < pref.length; i++) {
       if (pref[i] != word[i]) {
         result = false;
