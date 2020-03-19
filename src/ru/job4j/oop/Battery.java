585995bafbfe0e0a9bcec35356bbc2bffaf6e0c1
Mirto111 <mitritdil@ya.ru>
2020-03-20 00:36:54 +0300

    1.7. Состояние объекта.[#255690]

---
+++ src/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+  private int load;
+
+  public Battery(int load) {
+    this.load = load;
+  }
+
+  public void exchange(Battery another) {
+    this.load -= 1;
+    another.load += 1;
+  }
+
+  public static void main(String[] args) {
+    Battery first = new Battery(4);
+    Battery second = new Battery(5);
+    first.exchange(second);
+    System.out.println(("first : " + first.load + ". second : " + second.load));
+  }
+}
