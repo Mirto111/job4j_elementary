7489acb0710a134b4b5c16a504b2d5106d65551f
Mirto111 <mitritdil@ya.ru>
2020-03-20 03:13:07 +0300

    2.2. Рефакторинг - Площадь треугольника.[#255681]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -9,14 +10,38 @@ public class TriangleTest {
 
   @Test
   public void whenExist() {
-    boolean result = Triangle.isExist(2.0, 2.0, 2.0);
+    Point first = new Point(0, 0);
+    Point second = new Point(0, 2);
+    Point third = new Point(5, 0);
+    double a = first.distance(second);
+    double b = first.distance(third);
+    double c = second.distance(third);
+    Triangle triangle = new Triangle(first, second, third);
+    boolean result = triangle.isExist(a, b, c);
     assertThat(result, is(true));
   }
 
   @Test
   public void whenDoesNotExist() {
-    boolean result = Triangle.isExist(2.0, 2.0, 6.0);
+    Point first = new Point(0, 0);
+    Point second = new Point(0, 2);
+    Point third = new Point(0, 1);
+    double a = first.distance(second);
+    double b = first.distance(third);
+    double c = second.distance(third);
+    Triangle triangle = new Triangle(first, second, third);
+    boolean result = triangle.isExist(a, b, c);
     assertThat(result, is(false));
   }
 
+  @Test
+  public void area() {
+    Point first = new Point(0, 0);
+    Point second = new Point(1, 2);
+    Point third = new Point(5, 1);
+    Triangle triangle = new Triangle(first, second, third);
+    double result = triangle.area();
+    double expected = 4.5;
+    assertEquals(expected, result, 0.01);
+  }
 }
