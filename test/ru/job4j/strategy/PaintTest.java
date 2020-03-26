6b392d93fbd9a1d575f2b1b945c406e7ad1cc01c
Mirto111 <mitritdil@ya.ru>
2020-03-26 03:25:23 +0300

    9.1 Рефакторинг теста @Before @After[#255642]

--- test/ru/job4j/strategy/PaintTest.java
+++ test/ru/job4j/strategy/PaintTest.java
@@ -6,15 +6,27 @@ import static org.junit.Assert.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.StringJoiner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PaintTest {
 
+  private final PrintStream stdout = System.out;
+  private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+  @Before
+  public void loadOutput() {
+    System.setOut(new PrintStream(this.out));
+  }
+
+  @After
+  public void backOutput() {
+    System.setOut(this.stdout);
+  }
+
   @Test
   public void whenDrawSquare() {
-    PrintStream stdout = System.out;
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(out));
     new Paint().draw(new Square());
     assertThat(
         new String(out.toByteArray()),
@@ -27,14 +39,10 @@ public class PaintTest {
                 .toString()
         )
     );
-    System.setOut(stdout);
   }
 
   @Test
   public void whenDrawTriangle() {
-    PrintStream stdout = System.out;
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(out));
     new Paint().draw(new Triangle());
     assertThat(
         new String(out.toByteArray()),
@@ -47,6 +55,5 @@ public class PaintTest {
                 .toString()
         )
     );
-    System.setOut(stdout);
   }
 }
