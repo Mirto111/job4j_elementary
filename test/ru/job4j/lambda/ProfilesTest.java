51b3630e1038b43d591a9e1649a68d361be6e7d5
Mirto111 <mitritdil@ya.ru>
2020-04-03 14:42:35 +0300

    3. Уникальность элементов и сортировка.[#255621]

--- test/ru/job4j/lambda/ProfilesTest.java
+++ test/ru/job4j/lambda/ProfilesTest.java
@@ -20,4 +20,16 @@ public class ProfilesTest {
     List<Address> result = new Profiles().collect(List.of(prof, prof1, prof2));
     assertThat(result, is(Arrays.asList(adr, adr1, adr2)));
   }
+
+  @Test
+  public void onlyUniqueElements() {
+    Address adr = new Address("Moscow", "Griboedova", 15, 34);
+    Address adr1 = new Address("Bryansk", "Lenina", 24, 15);
+    Address adr2 = new Address("Bryansk", "Lenina", 24, 15);
+    Profile prof = new Profile(adr);
+    Profile prof1 = new Profile(adr1);
+    Profile prof2 = new Profile(adr2);
+    List<Address> result = new Profiles().collect(List.of(prof, prof1, prof2));
+    assertThat(result, is(Arrays.asList(adr1, adr)));
+  }
 }
